a7121f85ab35e35011822593d05f54e3d47f29dc
MLi-dev <limatthew2006@gmail.com>
2023-07-06 16:37:10 -0700

    Added functional calculator buttons

--- src/component/content/calculator.jsx
+++ src/component/content/calculator.jsx
@@ -1,37 +1,82 @@
 import React, { Component } from "react";
 import Base from "./base";
+import { connect } from "react-redux";
+import DigitButton from "./calculator/digitButton";
+import ACTIONS from "../../redux/action";
+import OperationButton from "./calculator/operationButton";
 
 class Calculator extends React.Component {
+	state = {
+		formatter: Intl.NumberFormat("en-us"),
+	};
+	format = (number) => {
+		const [integer, decimal] = number.split(".");
+		if (decimal === undefined) return this.state.formatter.format(integer);
+		return `${this.state.formatter.format(integer)}.${decimal}`;
+	};
+
 	render() {
 		return (
 			<Base>
 				<div className='calculator'>
 					<div className='output'>
-						<div className='last-output'> 123 *</div>
-						<div className='current-output'>9699696</div>
+						<div className='last-output'>
+							{this.format(this.props.lastOperand)} {this.props.operation}
+						</div>
+						<div className='current-output'>
+							{this.format(this.props.currentOperand)}
+						</div>
 					</div>
-					<button className='button-ac'>AC</button>
-					<button>Del</button>
-					<button>/</button>
-					<button>7</button>
-					<button>8</button>
-					<button>9</button>
-					<button>*</button>
-					<button>4</button>
-					<button>5</button>
-					<button>6</button>
-					<button>-</button>
-					<button>1</button>
-					<button>2</button>
-					<button>3</button>
-					<button>+</button>
-					<button>0</button>
-					<button>.</button>
-					<button className='button-equal'>=</button>
+					<button className='button-ac' onClick={this.props.clear_all}>
+						AC
+					</button>
+					<button onClick={this.props.delete_digit}>Del</button>
+					<OperationButton operation={"/"} />
+					<DigitButton digit={"7"} />
+					<DigitButton digit={"9"} />
+					<DigitButton digit={"8"} />
+					<OperationButton operation={"*"} />
+					<DigitButton digit={"4"} />
+					<DigitButton digit={"5"} />
+					<DigitButton digit={"6"} />
+					<OperationButton operation={"-"} />
+					<DigitButton digit={"1"} />
+					<DigitButton digit={"2"} />
+					<DigitButton digit={"3"} />
+					<OperationButton operation={"+"} />
+					<DigitButton digit={"0"} />
+					<DigitButton digit={"."} />
+					<button className='button-equal' onClick={this.props.evaluate}>
+						=
+					</button>
 				</div>
 			</Base>
 		);
 	}
 }
+const mapStateToProps = (state, props) => {
+	return {
+		currentOperand: state.currentOperand,
+		lastOperand: state.lastOperand,
+		operation: state.operation,
+	};
+};
+const mapDispatchToProps = {
+	delete_digit: () => {
+		return {
+			type: ACTIONS.DELETE_DIGIT,
+		};
+	},
+	clear_all: () => {
+		return {
+			type: ACTIONS.CLEAR,
+		};
+	},
+	evaluate: () => {
+		return {
+			type: ACTIONS.EVALUATE,
+		};
+	},
+};
 
-export default Calculator;
+export default connect(mapStateToProps, mapDispatchToProps)(Calculator);
